6cb520f41bfcc1b3c84e320ff87ffad6c62b9030
Vartika Goel <78904331+vartikagoel01@users.noreply.github.com>
2023-10-20 00:33:25 +0530

    Update program1.java

--- program1.java
+++ program1.java
@@ -1,65 +1,106 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
- public static void main(String[] args) {
- // A. Create an array with the values (1, 2, 3, 4, 5, 6, 7) and shuffle it.
- List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
- Collections.shuffle(numbers);
- System.out.println("Shuffled Array: " + numbers);
- // B. Enter Roman Number as input and convert it to an integer. (ex IX = 9)
- Scanner scanner = new Scanner(System.in);
- System.out.print("Enter a Roman numeral: ");
- String romanNumeral = scanner.nextLine();
- int decimalValue = romanToDecimal(romanNumeral);
- System.out.println("Decimal equivalent: " + decimalValue);
- // C. Check if the input is a pangram or not.
- System.out.print("Enter a sentence: ");
- String sentence = scanner.nextLine();
- boolean isPangram = checkPangram(sentence);
- if (isPangram) {
- System.out.println("The input is a pangram.");
- } else {
- System.out.println("The input is not a pangram.");
- }
- scanner.close();
- }
- // B. Roman numeral to decimal conversion function
- public static int romanToDecimal(String s) {
- Map<Character, Integer> romanMap = new HashMap<>();
- romanMap.put('I', 1);
- romanMap.put('V', 5);
- romanMap.put('X', 10);
- romanMap.put('L', 50);
- romanMap.put('C', 100);
- romanMap.put('D', 500);
- romanMap.put('M', 1000);
- int result = 0;
- int prevValue = 0;
- for (int i = s.length() - 1; i >= 0; i--) {
- int currentValue = romanMap.get(s.charAt(i));
- if (currentValue < prevValue) {
- result -= currentValue;
- } else {
- result += currentValue;
- }
- prevValue = currentValue;
- }
- return result;
- }
- // C. Pangram checking function
- public static boolean checkPangram(String sentence) {
- sentence = sentence.toLowerCase();
- boolean[] isAlphabetPresent = new boolean[26];
- for (int i = 0; i < sentence.length(); i++) {
- char c = sentence.charAt(i);
- if ('a' <= c && c <= 'z') {
- isAlphabetPresent[c - 'a'] = true;
- }
- }
- for (boolean alphabet : isAlphabetPresent) {
- if (!alphabet) {
- return false;
- }
- }
- return true;
- }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        System.out.print("Shuffled Array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.print("Enter a Roman Numeral: ");
+        String romanNumeral = scanner.nextLine();
+        int integerEquivalent = romanToInteger(romanNumeral);
+        System.out.println("Integer Equivalent: " + integerEquivalent);
+
+        System.out.print("Enter a sentence to check for a pangram: ");
+        String sentence = scanner.nextLine();
+        boolean isPangram = isPangram(sentence);
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            // Swap elements at i and j
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public static int romanToInteger(String romanNumeral) {
+        int result = 0;
+        int previousValue = 0;
+        
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            char c = romanNumeral.charAt(i);
+            int value = romanDigitValue(c);
+            
+            if (value < previousValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+            
+            previousValue = value;
+        }
+        
+        return result;
+    }
+
+    public static int romanDigitValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0; // Invalid Roman numeral
+        }
+    }
+
+
+    public static boolean isPangram(String sentence) {
+        sentence = sentence.toLowerCase();
+        boolean[] alphabet = new boolean[26];
+        int totalAlphabets = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (Character.isLetter(c)) {
+                int index = c - 'a';
+                if (!alphabet[index]) {
+                    alphabet[index] = true;
+                    totalAlphabets++;
+                }
+            }
+
+            if (totalAlphabets == 26) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
